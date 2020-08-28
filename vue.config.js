2d3cebfb71d5f0aef11cf2fc00224ee4beb3d21a
Henry <1309658793@qq.com>
2020-08-28 10:51:23 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // 项目部署的基础路径
-  publicPath: isProd,
+  publicPath: isProd ? './' : '/',
 
   // 将构建好的文件输出到哪里
   outputDir: '../../../resources/web/replaceOutputDir',
