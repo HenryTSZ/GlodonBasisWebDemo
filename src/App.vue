dd3af48eabf3b72e3eb7216db9728c991adadbe9
Henry <1309658793@qq.com>
2020-11-30 17:05:01 +0800

    替换 vuex

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,29 @@
 import { mutations } from 'store'
 export default {
   name: 'App',
-  mounted() {
-    mutations.setBodyWidth(document.body.clientWidth)
-    mutations.setBodyHeight(document.body.clientHeight)
-    window.onresize = () => {
+  methods: {
+    resize() {
       mutations.setBodyWidth(document.body.clientWidth)
       mutations.setBodyHeight(document.body.clientHeight)
+    },
+    debounce(fn, delay = 500) {
+      let timer
+      return function() {
+        let th = this
+        let args = arguments
+        if (timer) {
+          clearTimeout(timer)
+        }
+        timer = setTimeout(function() {
+          timer = null
+          fn.apply(th, args)
+        }, delay)
+      }
     }
+  },
+  mounted() {
+    this.resize()
+    window.onresize = this.debounce(this.resize)
   }
 }
 </script>
